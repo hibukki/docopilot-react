a4c8b5779c356a1c766051500de636d855fa52be
Yonatan Cale <myselfandfredy@gmail.com>
2025-04-22 13:33:06 +0300

    rename

--- src/client/sidebar-about-page/index.jsx
+++ src/client/sidebar-about-page/index.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
-import About from './components/About';
+import Docopilot from './components/About';
 
 const container = document.getElementById('index');
 const root = createRoot(container);
-root.render(<About />);
+root.render(<Docopilot />);
