f7aac4398d90bd9be18c24ef54b8795ac6b0639c
Yonatan Cale <myselfandfredy@gmail.com>
2025-04-22 13:04:37 +0300

    ui: support docs, not just sheet

--- src/server/ui.js
+++ src/server/ui.js
@@ -1,5 +1,9 @@
+const getUi = () => {
+  return DocumentApp.getUi();
+};
+
 export const onOpen = () => {
-  const menu = SpreadsheetApp.getUi()
+  const menu = getUi()
     .createMenu('My Sample React Project') // edit me!
     .addItem('Sheet Editor', 'openDialog')
     .addItem('Sheet Editor (Bootstrap)', 'openDialogBootstrap')
@@ -14,31 +18,31 @@ export const openDialog = () => {
   const html = HtmlService.createHtmlOutputFromFile('dialog-demo')
     .setWidth(600)
     .setHeight(600);
-  SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor');
+  getUi().showModalDialog(html, 'Sheet Editor');
 };
 
 export const openDialogBootstrap = () => {
   const html = HtmlService.createHtmlOutputFromFile('dialog-demo-bootstrap')
     .setWidth(600)
     .setHeight(600);
-  SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor (Bootstrap)');
+  getUi().showModalDialog(html, 'Sheet Editor (Bootstrap)');
 };
 
 export const openDialogMUI = () => {
   const html = HtmlService.createHtmlOutputFromFile('dialog-demo-mui')
     .setWidth(600)
     .setHeight(600);
-  SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor (MUI)');
+  getUi().showModalDialog(html, 'Sheet Editor (MUI)');
 };
 
 export const openDialogTailwindCSS = () => {
   const html = HtmlService.createHtmlOutputFromFile('dialog-demo-tailwindcss')
     .setWidth(600)
     .setHeight(600);
-  SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor (Tailwind CSS)');
+  getUi().showModalDialog(html, 'Sheet Editor (Tailwind CSS)');
 };
 
 export const openAboutSidebar = () => {
   const html = HtmlService.createHtmlOutputFromFile('sidebar-about-page');
-  SpreadsheetApp.getUi().showSidebar(html);
+  getUi().showSidebar(html);
 };
